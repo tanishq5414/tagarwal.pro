e5759c4dfbfe6762d0122b25f8a4dcd5ada83b25
tanishq5414 <tanishqagarwal5414@gmail.com>
2025-02-02 18:19:06 +0530

    feat: fixing sitemap

--- src/utils/sitemap.ts
+++ src/utils/sitemap.ts
@@ -6,17 +6,17 @@ export function generateSitemap() {
     const posts = getBlogMetaData('blogs');
     const baseUrl = 'https://tagarwal.pro';
 
-    const sitemapEntries = posts.map(post => {
-        return `
-            <url>
-                <loc>${baseUrl}/blog/${post.slug}</loc>
-                <lastmod>${new Date(post.date || new Date()).toISOString()}</lastmod>
-            </url>
-        `;
-    }).join('');
+    const sitemapEntries = posts.map(post => `
+        <url>
+            <loc>${baseUrl}/blog/${post.slug}</loc>
+            <lastmod>${new Date(post.date || new Date()).toISOString().split('T')[0]}</lastmod>
+            <changefreq>monthly</changefreq>
+            <priority>0.7</priority>
+        </url>
+    `).join('');
 
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap-image/1.1">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         <url>
             <loc>${baseUrl}/</loc>
             <changefreq>daily</changefreq>
@@ -35,5 +35,5 @@ export function generateSitemap() {
         ${sitemapEntries}
     </urlset>`;
 
-    fs.writeFileSync(path.join(process.cwd(), 'public', 'sitemap.xml'), sitemap);
+    fs.writeFileSync(path.join(process.cwd(), 'public', 'sitemap.xml'), sitemap.trim());
 }
